27ee852ed4569791d7ec5efeb31216bb9754f09c
G.Woolley <woollygrant@gmail.com>
2025-03-14 14:50:48 +0000

    Layout fixes and more video support
    - Fix Portfolio grid item size
    - Fix divider
    - Fix Video preview image
    - Fix platform layout
    - Added unannounced platforms option
    - Added support for YouTube and Vimeo embed

--- _posts/2025-03-03-Chicken Run Eggstraction.markdown
+++ _posts/2025-03-03-Chicken Run Eggstraction.markdown
@@ -3,9 +3,10 @@ layout: default
 modal-id: CR2
 date: 2025-03-03
 img: CR2.png
+video: https://www.youtube.com/watch?v=EBDOVwlRBxo
 alt: Chicken Run 2 Eggstraction
 release: TBA
-platforms: TBA
+platforms: []
 client: Aardman Animations / Netflix
 category: QA Testing
 description: I'm currently wrapping up the final touches on this game as QA Lead!
--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -6,18 +6,41 @@ document.addEventListener('DOMContentLoaded', function() {
         preview.addEventListener('click', function() {
             var overlay = this.querySelector('.video-preview-overlay');
             var video = this.querySelector('video');
+            var iframe = this.querySelector('iframe');
             
             overlay.style.display = 'none';
-            video.style.display = 'block';
-            video.play();
+            
+            // Handle different video types
+            if (video) {
+                video.style.display = 'block';
+                video.play();
+            } else if (iframe) {
+                iframe.style.display = 'block';
+                
+                // For YouTube, modify src to autoplay
+                if (iframe.src.includes('youtube.com')) {
+                    var src = iframe.src;
+                    if (!src.includes('autoplay=1')) {
+                        iframe.src = src + (src.includes('?') ? '&' : '?') + 'autoplay=1';
+                    }
+                }
+                // For Vimeo, modify src to autoplay
+                else if (iframe.src.includes('vimeo.com')) {
+                    var src = iframe.src;
+                    if (!src.includes('autoplay=1')) {
+                        iframe.src = src + (src.includes('?') ? '&' : '?') + 'autoplay=1';
+                    }
+                }
+            }
         });
-
+        
         // Modal close event to reset video
         var modal = preview.closest('.portfolio-modal');
         if (modal) {
             modal.addEventListener('hidden.bs.modal', function() {
                 var overlay = preview.querySelector('.video-preview-overlay');
                 var video = preview.querySelector('video');
+                var iframe = preview.querySelector('iframe');
                 
                 if (video) {
                     video.pause();
@@ -26,6 +49,21 @@ document.addEventListener('DOMContentLoaded', function() {
                     // Revert to thumbnail
                     overlay.style.display = 'block';
                     video.style.display = 'none';
+                } else if (iframe) {
+                    // Stop YouTube/Vimeo by resetting src
+                    var currentSrc = iframe.src;
+                    iframe.src = '';
+                    
+                    // We use setTimeout to ensure the iframe is fully reset
+                    setTimeout(function() {
+                        // Remove autoplay parameter if present
+                        var newSrc = currentSrc.replace(/(\?|&)autoplay=1/, '');
+                        iframe.src = newSrc;
+                        
+                        // Revert to thumbnail
+                        overlay.style.display = 'block';
+                        iframe.style.display = 'none';
+                    }, 100);
                 }
             });
         }
@@ -36,6 +74,7 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function() {
     $('.portfolio-modal').on('hidden.bs.modal', function (e) {
         var video = $(this).find('video');
+        var iframe = $(this).find('iframe');
         var overlay = $(this).find('.video-preview-overlay');
         
         if (video.length) {
@@ -45,6 +84,21 @@ $(document).ready(function() {
             // Revert to thumbnail
             overlay.show();
             video.hide();
+        } else if (iframe.length) {
+            // Stop YouTube/Vimeo by resetting src
+            var currentSrc = iframe.attr('src');
+            iframe.attr('src', '');
+            
+            // We use setTimeout to ensure the iframe is fully reset
+            setTimeout(function() {
+                // Remove autoplay parameter if present
+                var newSrc = currentSrc.replace(/(\?|&)autoplay=1/, '');
+                iframe.attr('src', newSrc);
+                
+                // Revert to thumbnail
+                overlay.show();
+                iframe.hide();
+            }, 100);
         }
     });
 });
